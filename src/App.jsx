7f383c7a43fe7056b599626895a74d3e0373199d
andryaallen-web <andryaallen@gmail.com>
2026-03-15 12:01:16 -0500

    Fix App.jsx import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import React from 'react'
 import HALO from './HALO_Dashboard_v8'
+import './index.css'
 
-export default function App() {
+function App() {
   return <HALO />
 }
+
+export default App
